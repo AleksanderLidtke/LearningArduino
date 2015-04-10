eba32414ee1f9bf314a211d31b918b4158aac8e4
AleksanderLidtke <alek_l@onet.eu>
2015-04-10 21:30:32 +0100

    Updated the circuit to try and moake it suitable for 2N7000 but the current still flows through it.

--- mosfetDrivingLED/mosfetDrivingLED.ino
+++ mosfetDrivingLED/mosfetDrivingLED.ino
@@ -1,5 +1,5 @@
 int PIN_NO = 10; // Pin to which the MOSFET is connected.
-int DURATION = 10; // How long the wait at a given level of current going through the LED, ms.
+int DURATION = 500; // How long the wait at a given level of current going through the LED, ms.
 int FULL_CURRENT_DURATION = 1000; // How long to keep the current at the highest value.
 int PAUSE_DURATION = 500; // How much time to wait between consecutive ramp ups of current, ms.
 
@@ -13,13 +13,13 @@ void setup(void)
 void loop(void)
 {
   /* Called repeatedly whilst Arduino is active. */
-  analogWrite(PIN_NO, 255);
-  /*for(int level=0; level<256; level++) // Gradually ramp up the current going through the diode by 
+  for(int level=0; level<256; level++) // Gradually ramp up the current going through the diode by 
   {
     analogWrite(PIN_NO, level); // Set the pin a bit higher...
     delay(DURATION); // ...wait for a while to be able to see how the current is getting ramped up.
-  }*/
+  }
   delay(FULL_CURRENT_DURATION); // Wait for a while at full current.
+  
   analogWrite(PIN_NO, 0); // Set the pin back to low to cut the current.
   delay(PAUSE_DURATION); // Wait until the next cycle.
 }
